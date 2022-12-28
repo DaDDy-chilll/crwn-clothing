e190742e4594b74132c46ea38ef3887637f89543
DaDDy <blacklistuser420@gmail.com>
2022-12-28 11:19:47 +0630

    updating

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigatio.component";
+import SignIn from "./routes/sign-in/sign-in.component";
+const Shop = () => {
+  return <h1>i am the shop page</h1>;
+};
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="signin" element={<SignIn />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
